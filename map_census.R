e1ef75cea2a55880bf4c48bb205641a335bcf230
Cee Nell <cnell@usgs.gov>
2024-02-07 14:12:22 -0800

    combine acs and fema data

--- map_census.R
+++ map_census.R
@@ -13,7 +13,7 @@ census_vars <- tidycensus::load_variables(year = 2022, dataset = "acs1")
 census_vars
 
 # states of interest to pull and map data
-census_states <- c('Mississippi')
+census_states <- c('MS')
 proj <- 'EPSG:5070'
 
 # polygons for US states
@@ -21,6 +21,7 @@ states <- tigris::states(cb = TRUE) |>
     st_transform(proj) |>
     rmapshaper::ms_simplify(keep = 0.2) |> 
     filter(NAME %in% census_states)
+str(states)
 
 # census variable to pull in
 var_name <- "B01003_001"
@@ -29,29 +30,44 @@ var_name <- "B01003_001"
 df <- get_acs(
     geography = 'tract',
     variable = var_name,
-    state = 'MS',
+    state = census_states,
     year = 2022,
     geometry = TRUE) |>
     st_transform(proj)
 
-df
+str(df)
 
 
-## make a mpa of the census variable
+# risk data
+dat <-read.csv("NRI_Table_CensusTracts_Mississippi/NRI_Table_CensusTracts_Mississippi.csv") |>
+    transform(geoid = as.character(TRACTFIPS))
+
+names(dat)[names(dat) == 'STCOFIPS'] <- 'fips'
+
+#$ combine risk and census data
+risk_census <- df |>
+    left_join(dat)
+risk_census
+
+
+## make a map 
 library(scico) # color scales
 
-df |>
+risk_census |>
     ggplot() +
-    geom_sf(aes(fill = estimate), 
+    geom_sf(aes(fill = RISK_SCORE), 
             color = NA) +
     theme_void() +
     theme(legend.position = 'bottom') +
     guides(fill = guide_colorbar(
-        title = "Total population",
+        title = "Risk",
         title.position = "top",
+        title.theme = element_text(face = 'bold'),
         direction = "horizontal", 
         position = "bottom", 
         barwidth = 20, 
         barheight = 1
     )) +
-    scale_fill_scico(palette ='bilbao')
+    scale_fill_scico(palette ='acton',
+                     direction = -1,
+                     labels = scales::label_comma()) 
